d3649bcccaa5abbd618ce90de3599dfbd614458c
Yorvin Luna Wisky <yorvin.luna@micm.gob.do>
2023-12-01 11:30:43 -0400

    version 1

---
+++ components/LoadMore.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import Image from "next/image";
+import { useInView } from "react-intersection-observer";
+import {useEffect, useState} from "react";
+import {fetchAnimate} from "@/app/actions";
+
+let page = 2;
+export type animateCard = JSX.Element
+
+function LoadMore() {
+    const { ref, inView } = useInView();
+    const [data, setData] = useState<animateCard[]>([]);
+    useEffect(() => {
+        if (inView) {
+            fetchAnimate(page).
+            then((res) => {
+               setData([...data, ...res]);
+                page++;
+            });
+        }
+    }, [inView,data]);
+
+  return (
+    <>
+        <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
+            {data}
+        </section>
+      <section className="flex justify-center items-center w-full">
+        <div ref={ref}>
+          <Image
+            src="./spinner.svg"
+            alt="spinner"
+            width={56}
+            height={56}
+            className="object-contain"
+          />
+        </div>
+      </section>
+    </>
+    );
+}
+
+export default LoadMore;
